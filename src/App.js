8a41fd501e530e1053ce864da2517e5e6fe28474
jackern <jackahern15@gmail.com>
2021-03-02 21:07:39 +0000

    update menu-item and components to use withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <Homepage />
+    <Switch>
+      <Route path='/' component={Homepage} />
+    </Switch>
   );
 }
 
