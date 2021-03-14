94399b659ac40c9b14eaa02325bbdc2b1c4053e5
jackern <jackahern15@gmail.com>
2021-03-14 19:20:41 +0000

    new header component added with styling, links to shop and contact added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Homepage} />
-      <Route path='/' component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
