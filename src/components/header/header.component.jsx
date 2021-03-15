d73d56580e1366be33ebf2189c3245eebd7bda9b
jackern <jackahern15@gmail.com>
2021-03-15 22:18:29 +0000

    sign in, sign out handled with google, sign in styled

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { auth } from '../../firebase/firebase.utils';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
     <div className="header">
-        <Link classname="logo-container" to="/">
+        <Link className="logo-container" to="/">
             <Logo className="logo" />
         </Link>
-        <div classname="options">
+        <div className="options">
             <Link className="option" to="/shop">
                 SHOP
             </Link>
             <Link className="option" to="/contact">
                 CONTACT
             </Link>
-            <Link className="option" to="/signin">
-                SIGN IN/REGISTER
-            </Link>
+            {currentUser ? (
+                <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+                ) : (
+                <Link className="option" to="/signin">
+                    SIGN IN
+                </Link>
+                )
+            }
         </div>
     </div>
 )
